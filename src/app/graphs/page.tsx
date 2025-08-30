f709f68b967301ff74466d88c1493e1ab33e16f8
Naratorn Pisedtasalasai <npis2542@uni.sydney.edu.au>
2025-08-30 19:12:33 +1000

    created a graph page and imported components

--- src/app/graphs/page.tsx
+++ src/app/graphs/page.tsx
@@ -1,18 +1,15 @@
-// app/graph/page.tsx
-import React from "react";
+import Graph from "../components/graphs";
+import Navbar from "../components/navbar";
+import Sidebar from "../components/sidebar";
 
-const GraphPage = () => {
+export default function GraphsPage() {
   return (
-    <div style={{ width: "100%", height: "100vh", textAlign: "center" }}>
-      <h1>Sine Wave Plot</h1>
-      <iframe
-        src="http://127.0.0.1:8000/plot/sine" // your FastAPI endpoint
-        width="800"
-        height="600"
-        style={{ border: "1px solid #ccc", marginTop: "20px" }}
-      ></iframe>
+    <div className="flex h-screen">
+      <Sidebar /> {/* collapsible sidebar */}
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        <Graph />
+      </div>
     </div>
   );
-};
-
-export default GraphPage;
+}
